d69b7890c3ccdfc2aa34cfceb276991a03fa9b51
nicowaismann <46961446@est.ort.edu.ar>
2022-03-23 15:55:38 -0300

    Exersice 1 finished

--- Assets/Ejercicio01.cs
+++ Assets/Ejercicio01.cs
@@ -1,13 +1,22 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+
+//Realizá un programa donde se cree una variable entera llamada num1, 
+//que inicialmente  valdrá 0. Luego incrementá su valor en 2 y mostralo 
+//por pantalla.Después mostrá el  resultado de multiplicarlo por sí mismo. 
+
 using UnityEngine;
 
 public class Ejercicio01 : MonoBehaviour
+    
 {
+    int num1 = 0;
     // Start is called before the first frame update
     void Start()
     {
-        
+        num1 += 2;
+        Debug.Log(num1);
+        Debug.Log(num1 * num1);
     }
 
     // Update is called once per frame

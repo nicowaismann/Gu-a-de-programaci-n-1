6610226829081485239c3c71ad9d02d5d4c342ff
nicowaismann <46961446@est.ort.edu.ar>
2022-03-23 19:26:42 -0300

    Exercise 3 finished

---
+++ Assets/ejercicio3.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ejercicio3 : MonoBehaviour
+{
+    string palabra = "Nicolás";
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("Hola " + palabra);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}

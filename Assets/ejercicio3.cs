70a354ad847bc13d491c44c084bd7998a1acdd87
nicowaismann <46961446@est.ort.edu.ar>
2022-03-23 20:09:00 -0300

    Exercise 4 finished

--- Assets/ejercicio3.cs
+++ Assets/ejercicio3.cs
@@ -2,6 +2,11 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+//Crear una variable pública que permita ingresar texto. Permitir que al ejecutar el programa
+//se muestre en la consola un mensaje de saludo que incluya el texto de la variable. Por
+//ejemplo, si en la variable se ingresa el texto “Ana”, el mensaje en consola debe decir “Hola
+//Ana”. 
+
 public class ejercicio3 : MonoBehaviour
 {
     string palabra = "Nicolás";
---
+++ Assets/ejercicio4.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//Realizá un programa que permita ingresar dos números enteros num1 y num2 por
+//Inspector. Luego, mostrar el resultado de la suma entre ambos al ejecutar la escena
+//utilizando el siguiente formato: “La suma entre “ + num1 + “ y “ + num2 + “ da como
+//resultado “ + num1 + num2.
+
+public class ejercicio4 : MonoBehaviour
+{
+    public int num1;
+    public int num2;
+    int num3;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        num3 = num1 + num2;
+        Debug.Log("La suma entre " + num1 + " y " + num2 + " da como resultado " + num3);
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}

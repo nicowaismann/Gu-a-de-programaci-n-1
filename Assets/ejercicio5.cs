47ff5e2c94c95fb7a5e19eff74c95d7f6ee58c0c
nicowaismann <46961446@est.ort.edu.ar>
2022-03-23 20:54:26 -0300

    Ej 6 casi terminado
    Falta poner que sea mayor a 0 el numero

---
+++ Assets/ejercicio5.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+//Realizá un programa que, dados dos números enteros num1 y num2 cargados desde el
+//Inspector muestre el resultado de la división del primero por el segundo en la consola
+//siguiendo el formato num1 + “ / “ + num2 + “ = “ + resultado.
+//En el caso de que el segundo valor num2 sea cero, mostrar el siguiente mensaje de error:
+//“No se puede dividir por cero”.
+
+public class ejercicio5 : MonoBehaviour
+{
+    public double num1;
+    public double num2;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (num2 == 0)
+        {
+            Debug.Log("No se puede dividir por cero");
+        }
+        else
+        {
+            Debug.Log(num1 + "/" + num2 + "=" + num1 / num2);
+        }
+
+        
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/ejercicio6.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+//Realizá un programa que permita el ingreso por Inspector de un valor entero mayor que 0
+//en una variable llamada num1 y muestre un mensaje por pantalla indicando  "el número es
+//par" o  "el número es impar". Deberá utilizar el operador “módulo” es el caracter %.
+
+public class ejercicio6 : MonoBehaviour
+{
+    public int num1;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+        if (num1 % 2 == 0)
+        {
+            Debug.Log("El numero es par");
+        }
+
+        else
+        {
+            Debug.Log("El numero es impar");
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}

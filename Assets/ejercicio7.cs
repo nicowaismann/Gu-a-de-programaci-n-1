092dc5820d6796c9d3f548f5f0bf22caca9f5fe7
nicowaismann <46961446@est.ort.edu.ar>
2022-03-23 22:55:10 -0300

    Ejercicios 7 y 8 terminados

---
+++ Assets/ejercicio7.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//Realizá un programa que al ingresar por Inspector tres números enteros num1, num2 y  num3, muestre el valor del mayor de todos. 
+//En caso de igualdad entre los tres imprimir "Los números son iguales".  Test1: num1 = 12, num2 = 4, num3 = 7.Resultado esperado: 
+//El mayor número es num1  Test2: num1 = 2, num2 = 65, num3 = 8.Resultado esperado: El mayor número es num2 
+//Test3: num1 = 3, num2 = 10, num3 = 28.Resultado esperado: El mayor número es num3  Test4: num1 = 5, num2 = 5, num3 = 5.Resultado esperado: Los números son iguales
+
+
+public class ejercicio7 : MonoBehaviour
+{
+    public int num1;
+    public int num2;
+    public int num3;
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (num1==num2 && num2==num3)
+        {
+            Debug.Log("Los numeros son iguales");
+        }
+
+        if(num1>num2 && num1 > num3)
+        {
+            Debug.Log(num1 + " es el número mas grande");
+        }
+
+        if (num2 > num1 && num2 > num3)
+        {
+            Debug.Log(num2 + " es el número mas grande");
+        }
+
+        if (num3 > num1 && num3 > num2)
+        {
+            Debug.Log(num3 + " es el número mas grande");
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/ejercicio8.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ejercicio8 : MonoBehaviour
+{
+
+    public int día;
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (día == 1)
+        {
+            Debug.Log("Domingo");
+        }
+
+        else if (día == 2)
+        {
+            Debug.Log("Lunes");
+        }
+
+        else if (día == 3)
+        {
+            Debug.Log("Martes");
+        }
+
+        else if (día == 4)
+        {
+            Debug.Log("Miércoles");
+        }
+
+        else if (día == 5)
+        {
+            Debug.Log("Jueves");
+        }
+
+        else if (día == 6)
+        {
+            Debug.Log("Viernes");
+        }
+
+        else if (día == 7)
+        {
+            Debug.Log("Sábado");
+        }
+
+
+        else
+        {
+            Debug.Log("El día ingresado no es válido");
+        }
+
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}

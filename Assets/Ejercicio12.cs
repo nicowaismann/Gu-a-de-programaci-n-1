d2ac52b939088bb2884bb90bd7865fd1911f14ce
nicowaismann <46961446@est.ort.edu.ar>
2022-03-29 08:50:27 -0300

    Exercise 12 finished

---
+++ Assets/Ejercicio12.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ejercicio12 : MonoBehaviour
+{
+    int dado1;
+    int dado2;
+    int dado3;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        dado1 = Random.Range(1, 7);
+        dado2 = Random.Range(1, 7);
+        dado3 = Random.Range(1, 7);
+
+        Debug.Log(dado1 +  " " + dado2 + " " + dado3);
+
+        if (dado1 == 6 && dado2 == 6 && dado3 == 6)
+        {
+            Debug.Log("Excelente");
+        }
+       
+        else if(dado1==6 && dado2==6 || dado1==6 && dado3==6 || dado2==6 && dado3 == 6){
+            Debug.Log("Muy Bien");
+        }
+
+        else if(dado1==6 || dado2==6 || dado3 == 6)
+        {
+            Debug.Log("Regular");
+        }
+
+        else
+        {
+            Debug.Log("Insuficiente");
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
